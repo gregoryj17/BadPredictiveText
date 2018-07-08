823c9ef6671a09776c1a4effc563011112862503
gregoryj17 <jaxtaxman@gmail.com>
2018-07-07 21:31:19 -0600

    Add project files.

---
+++ MashupCreator/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+
+namespace BadPredictiveText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Boolean moreInput = true;
+            String filename;
+            Dictionary<String, Word> dict = new Dictionary<string, Word>();
+
+            Console.Write("What file should text be read from?: ");
+            filename = Console.ReadLine();
+
+
+            while (moreInput)
+            {
+                if (String.IsNullOrWhiteSpace(filename))
+                {
+                    moreInput = false;
+                    continue;
+                }
+                //string[] lines = System.IO.File.ReadAllLines("Text.txt");
+                if (filename.IndexOf('.') < 0)
+                {
+                    filename = filename + ".txt";
+                }
+                string lines = System.IO.File.ReadAllText(filename);
+
+                while (lines.IndexOf("[") > -1)
+                {
+                    int start = lines.IndexOf("[");
+                    int end = lines.IndexOf("]");
+                    lines = lines.Replace(lines.Substring(start, (end - start + 1)), "");
+                }
+                //Console.WriteLine(lines);
+                string[] words = lines.Split(' ', ',', '.', '\n', '(', ')', '!', ';', '?', ':', '-', '"');
+                
+                Word prevword = null;
+                foreach (string word in words)
+                {
+                    if (word == "" || word == " " || word == "\n" || String.IsNullOrWhiteSpace(word))
+                    {
+                        continue;
+                    }
+                    if (prevword != null)
+                    {
+                        prevword.addNextWord(word);
+                    }
+                    prevword = null;
+                    if (word != "END")
+                    {
+                        if (!dict.ContainsKey(word.ToLower()))
+                        {
+                            dict.Add(word.ToLower(), new Word(word));
+                        }
+                        dict.TryGetValue(word.ToLower(), out prevword);
+                    }
+                }
+
+                Console.WriteLine("Input processing finished.");
+                Console.Write("What file should text be read from next? (Submit blank line if finished): ");
+                filename = Console.ReadLine();
+
+            }
+
+            Console.WriteLine("Generating output...");
+
+            String w = "we";
+            Word wor;
+            StreamWriter sw = new StreamWriter("song.txt");
+            while(w!="END")
+            {
+                sw.Write(w + " ");
+                dict.TryGetValue(w, out wor);
+                w = wor.getNextWord();
+                Thread.Sleep(15);
+            }
+            sw.Flush();
+            Console.WriteLine("Output processing finished.");
+            #if DEBUG
+            Console.WriteLine("Press enter to close...");
+            Console.ReadLine();
+            #endif
+        }
+    }
+}
---
+++ MashupCreator/Word.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BadPredictiveText
+{
+    public class Word
+    {
+        String word;
+        List<String> nextWords;
+
+        public Word()
+        {
+            nextWords = new List<String>();
+        }
+
+        public Word(String w)
+        {
+            word = w.ToLower();
+            nextWords = new List<String>();
+        }
+
+        public Word(String w, params String[] words)
+        {
+            word = w.ToLower();
+            nextWords = new List<String>();
+            foreach(string wo in words)
+            {
+                addNextWord(wo);
+            }
+        }
+
+        public String getWord()
+        {
+            return word;
+        }
+
+        public void addNextWord(String newWord)
+        {
+            if (newWord != "END")
+            {
+                nextWords.Add(newWord.ToLower());
+            }
+            else
+            {
+                nextWords.Add(newWord);
+            }
+        }
+
+        public void addNextWords(params String[] words)
+        {
+            foreach (String w in words)
+            {
+                addNextWord(w);
+            }
+        }
+        public String getNextWord()
+        {
+            Random rand = new Random();
+            return nextWords[rand.Next(nextWords.Count)];
+        }
+    }
+}
